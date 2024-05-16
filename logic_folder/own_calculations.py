da670b3b2a4fdf731c1fee52210083fcdafd3a51
Tristan <tristan.haess@gmail.com>
2024-05-16 20:48:54 +0200

    Added a while loop for infinite calculations Added some error handlings

--- logic_folder/own_calculations.py
+++ logic_folder/own_calculations.py
@@ -1,34 +1,44 @@
 from logic_folder import dependencies
 
+results = []
+last_result = 0
+
 
 def own_calculation(calculation):
-    global result
+    global temp
     try:
-        result = eval(calculation)
+        temp = eval(calculation)
     except Exception as e:
         print("An Error occurred:", e)
-    return result
+    return temp
 
 
 def calculate_with_last_result(last, calculation):
-    global result
+    global temp
     try:
-        test = str(last) + calculation
-        result = eval(test)
+        evaluation = str(last) + calculation
+        temp = eval(evaluation)
     except Exception as e:
         print("An Error occurred:", e)
-    return result
+    return temp
+
+
+def result_array(result, array="results"):
+    if len(array) <= 9:
+        results.append(result)
+    else:
+        results.pop(0)
+        results.append(result)
+    return results
 
 
 def main():
+    global results
     global last_result
-    global result_array
-    last_result = 0
-    result_array = []
     cancel = False
     while not cancel:
-        print("\n1. Input your own calculation")
-        print("2. Calculate with your last result (start your equation with an operator)")
+        print("\n1. Do your own calculation")
+        print("2. Calculate with your last result")
         print("3. Print the last result")
         print("4. Choose one of the last 10 results, with you want to calculate again")
         print("5. Exit")
@@ -37,25 +47,35 @@ def main():
             break
         match choice:
             case "1":
-                calculation = input("Input your equation: ")
-                last_result = own_calculation(calculation)
-                print(f"The result of {calculation} is: {own_calculation(calculation)}")
-                result_array.append(own_calculation(calculation))
+                while True:
+                    calculation = input("Input calculation (q for quit): ")
+                    if calculation == "q":
+                        break
+                    else:
+                        last_result = own_calculation(calculation)
+                        print(f"The result of {calculation} is: {own_calculation(calculation)}\n")
+                        result_array(own_calculation(calculation))
             case "2":
-                calculation = input("Input your equation: ")
-                print(
-                    f"The result of {last_result}{calculation} is: {calculate_with_last_result(last_result, calculation)}")
+                calculation = input("Your equation has to start with an operator: ")
+                print(f"The result of {last_result}{calculation} is: "
+                      f"{calculate_with_last_result(last_result, calculation)}")
                 last_result = calculate_with_last_result(last_result, calculation)
-                result_array.append(calculate_with_last_result(last_result, calculation))
+                result_array(calculate_with_last_result(last_result, calculation))
             case "3":
-                print("Your last result was: ", last_result)
+                print("Your last result is: ", last_result)
             case "4":
-                print("Your last results are: ", result_array)
-                choice = input("Which result would you like to calculate again?: ")
-                menu = dependencies.check(choice)
-                menu -= 1
-                print(f"{result_array[menu]} is now your last result. Choose menu number 2. to calculate again. "
-                      f"Otherwise it will be overwritten.")
-                last_result = result_array[menu]
+                if len(results) == 0:
+                    print("There are no results in your history")
+                else:
+                    print(f"Your last results are: {results}")
+                    choice = input("Which result would you like to calculate again?: ")
+                    menu = dependencies.check(choice)
+                    menu -= 1
+                    if menu >= len(results):
+                        print("\nIndex is out of range")
+                    else:
+                        print(f"{results[menu]} is now your last result. Choose menu number 2. to calculate again. "
+                              f"Otherwise it will be overwritten.")
+                        last_result = results[menu]
             case _:
                 print("Invalid input please try again")
